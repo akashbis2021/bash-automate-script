5b8a0a944c1e1f7e081525c2268d704fa171dbd7
Akash Biswas INDIAN <96134576+akashbis2021@users.noreply.github.com>
2023-02-03 02:42:44 +0530

    Add files via upload
    this is an automatescript of wifi operation all wifi operation in one file
    Scanning network's, connecting and disconnecting network's, wifi module on and off , showing wifi model.

---
+++ scripts/wifi-module.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+echo " "
+echo "Wifi-Module-Option:"
+echo "1. Wifi-Info"
+echo "2. Set-Wifi-Module"
+echo "3. Wifi-Scan-Networks"
+echo "4. Wifi-Connect"
+echo "5. Wifi-Disconnect"
+echo "6. Wifi-turn(On/Off)" 
+echo "7. Quit"
+echo " "
+read -p "Enter Option:" choice
+
+
+while  :
+do
+
+ case $choice in
+
+  1)clear
+    echo "Wifi-Info:"
+    echo " "
+    iwconfig
+    echo " ";;
+  
+  2)clear
+    read -p "Set-Wifi-Module:" wifimodel
+    echo "Wifi-Module is Set... "
+    echo " ";;
+
+  3)clear
+    echo "Wifi-Scanned-Networks:"
+    iwlist $wifimodule scan | grep ESSID
+    echo " ";;
+
+  4)clear
+    echo "Wifi-Connect..."
+    read -p "Enter ESSID: " essid 
+    nmcli d wifi connect $essid
+    clear
+    echo "$essid is succesfully connect.."
+    echo " ";;
+
+  5)clear
+    echo "Wifi-Disconnect..."
+    nmcli d disconnect $wifimodel
+    echo " ";;
+
+  6)clear
+    read -p "Wifi-Module(On/Off):" switch
+    if [ $switch == "on" ]
+    then
+       nmcli d  connect $wifimodel
+       clear
+       echo "Wifi is Connected.. .."
+    elif [ $switch == "off" ]
+    then
+       nmcli d disconnect $wifimodel
+       clear
+       echo "Wifi is Disconnect.. .."
+    fi
+    echo " ";;
+
+  7)clear
+    echo "Quit-module"
+    echo " "
+    exit;;
+
+  *)clear
+    echo "Invalid Option.."
+    echo " ";;
+esac
+
+echo " "
+echo "Wifi-Module-Option:"
+echo "1. Wifi-Info"
+echo "2. Set-Wifi-Module"
+echo "3. Wifi-Scan-Networks"
+echo "4. Wifi-Connect"
+echo "5. Wifi-Disconnect"
+echo "6. Wifi-turn(On/Off)" 
+echo "7. Quit"
+echo " "
+read -p "Enter Option:" choice
+
+
+done
